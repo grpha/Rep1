9801f301bba1d144db43777658aec7ceeadab4c1
Конькова Ульяна <goringennady11@gmail.com>
2022-02-10 23:45:03 +0300

    Добавьте файлы проекта.

---
+++ Rep1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rep1
+{
+    class MyHello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MyHello");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}");
+            Console.ReadKey();
+        }
+    }
+}
