90e64435d311f7ad63881a7bd0ad2369eb2ee57e
Конькова Ульяна <goringennady11@gmail.com>
2022-02-14 09:36:06 +0300

    Комментарии + задача 2

---
+++ 2/2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args) //массив args
+        {
+            Console.WriteLine(args.Length); //вывод массива
+            int a = Convert.ToInt32(args[0]); //переменная а типа int
+            int b = Convert.ToInt32(args[1]); //переменная b типа int
+            Console.WriteLine(a + b); //сумма значений переменных
+        }
+    }
+}
---
+++ Rep1/1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rep1
+{
+    class MyHello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MyHello");  //вывод текста
+            string name; //переменная нейм типа стринг
+            name = Console.ReadLine(); //переменной присваевается значение
+            Console.WriteLine($"Привет, {name}"); //вывод
+            Console.ReadKey();
+        }
+    }
+}
